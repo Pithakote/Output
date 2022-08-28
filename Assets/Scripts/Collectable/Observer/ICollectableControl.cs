21427eb67421e9aeaf5aae61937c42a596eaa072
Anish Pithakote <muttsang@gmail.com>
2022-08-29 00:54:10 +0100

    Refactor question
    Added a new interface for CollectableController

---
+++ Assets/Scripts/Collectable/Observer/ICollectableControl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollectableControl
+{
+    //delegate void PickedUpEvent(GameObject gameObject);
+    event Action <GameObject> onPickedUpEvent;
+    void OnPickedUp(GameObject gameObject);
+}
