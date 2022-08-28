877d61f68884278910db9de7385487f7878a5732
Anish Pithakote <muttsang@gmail.com>
2022-08-28 15:07:19 +0100

    Fixed the bug where turning the hard mode does not set the health to half of the max health
    Set health was called in Awake() which set the value of Health to half of Max health. This was later un-done in the start function where the value of Health was set the same as the max health

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,20 +12,21 @@ public class GameController : MonoBehaviour
 	private UIController UIController;
 
 	private void Awake()
-	{
+	{		
+
+		cameraController = new CameraController(transform.Find("Camera").GetComponent<Camera>(), characterController);
+		collectableController = new CollectableController(transform.Find("Collectables"), characterController);
+		StarController starController = GetComponent<StarController>();
+		UIController = new UIController(transform.Find("UI"), characterController, starController);
+	}
+
+	void Start()
+    {
 		if (hardMode)
 		{
 			int startHealth = characterController.MaxHealth / 2;
 			characterController.SetHealth(startHealth);
 		}
-	}
-
-	void Start()
-    {
-		cameraController = new CameraController(transform.Find("Camera").GetComponent<Camera>(), characterController);
-		collectableController = new CollectableController(transform.Find("Collectables"), characterController);
-		StarController starController = GetComponent<StarController>();
-		UIController = new UIController(transform.Find("UI"), characterController, starController);
 	}
 
 	private void Update()
