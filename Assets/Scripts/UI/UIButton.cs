91a78a6cefd268c0670030da7fad9da9ee5138aa
Anish Pithakote <muttsang@gmail.com>
2022-08-29 21:32:37 +0100

    Added a separate UIButton class

---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Button))]
+public class UIButton : MonoBehaviour
+{
+    protected GameController gameControllerInstance = default;
+
+    protected Button uiButton;
+    private void Awake()
+    {
+        uiButton = GetComponent<Button>();
+    }
+
+    protected virtual void OnEnable()
+    {
+        uiButton.onClick.AddListener(onButtonClick);
+    }
+
+    protected virtual void Start()
+    {
+        gameControllerInstance = GameController.Instance;
+    }
+
+    protected virtual void OnDisable()
+    {
+        uiButton.onClick.RemoveListener(onButtonClick);
+    }
+
+    void onButtonClick()
+    {
+        gameControllerInstance.InventoryController.InventoryToggleFunc();
+    }
+}
