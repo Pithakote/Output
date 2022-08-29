0c7c4beba67327c5c82d57da4b59364e8f7f8211
Anish Pithakote <muttsang@gmail.com>
2022-08-29 16:09:30 +0100

    Added scriptable objects for collectable inventory items

---
+++ Assets/ScriptableObjects/CollectableInventoryItemsSO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InventoryType
+{ 
+    Weapon,
+    CraftWeapons,
+    CraftMagicalItems
+}
+
+[CreateAssetMenu(fileName = "Collectable Inventory Item SO", menuName = "Scriptable Object Custom/Colletable Inventory")]
+public class CollectableInventoryItemsSO : ScriptableObject
+{
+    public Sprite Image;
+    public string Name = default;
+    public string Description = default;
+    public Material MeshMaterial = default;
+    public Mesh ModelMesh;
+    public InventoryType InventoryTypeItem;
+}
